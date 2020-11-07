a76d70d24cae07fccf3d6f283f14b495e4b2a3cd
evan54 <evan54@gmail.com>
2020-11-07 09:28:35 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MySimpleGui',
+      version='1.0',
+      description='Additional helper to send email',
+      py_modules=['myemail'],
+      author='evan54',
+      )
+
